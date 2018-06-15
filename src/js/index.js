f473c60c666b190b37a6ef6ac4e3a2bb675de860
thomas <thomas.rachavong@affluences.com>
2018-06-15 14:50:02 +0200

    shader material

--- src/js/index.js
+++ src/js/index.js
@@ -5,48 +5,31 @@ import '../scss/index.scss';
 import * as THREE from 'three';
 import 'three/examples/js/controls/PointerLockControls';
 
+import baseFragment from '../shader/base.fragment.glsl';
+import baseVertex from '../shader/base.vertex.glsl';
+
 let camera, scene, renderer, geometry, material, mesh, controls;
 
-var earth,sun;
+var head;
 
 function init() {
     scene = new THREE.Scene();
 
+
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 
-    var sunGeometry = new THREE.SphereBufferGeometry( 5, 32, 32 );
-    var sunMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    sun = new THREE.Mesh( sunGeometry, sunMaterial );
+    var sunGeometry = new THREE.SphereBufferGeometry( 2, 32, 32 );
+    var sunMaterial = new THREE.ShaderMaterial( {
+        vertexShader : baseVertex,
+        fragmentShader : baseFragment
+    } );
+    head = new THREE.Mesh( sunGeometry, sunMaterial );
 
-    var earthGeometry = new THREE.SphereBufferGeometry( 2, 32, 32  );
-    var earthMaterial = new THREE.MeshPhongMaterial( { color: 0x009fdb, shininess: 100 } );
 
 
-    earth = new THREE.Mesh( earthGeometry, earthMaterial );
+    head.position.y = 10;
 
-    earth.position.x = 30;
-
-   var geometry = new THREE.BufferGeometry();
-   var vertices = new Float32Array([
-      -20, -20, 20,
-       20, -20, 20,
-       -20, 20, 20,
-        20,20,20
-   ]);
-
-   geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
-   var material = new THREE.MeshBasicMaterial({ color : 0xff0000, side : THREE.DoubleSide});
-   var mesh = new THREE.Mesh(geometry,material);
-
-   scene.add(mesh);
-
-    sun.add(earth);
-    scene.add( sun );
-    scene.add( earth );
-
-    var light = new THREE.PointLight( 0xffffff, 1, 5000 );
-    light.position.set( 0, 0, 0 );
-    scene.add( light );
+    scene.add( head );
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -76,6 +59,11 @@ function init() {
         element.requestPointerLock();
     }, false);
 
+    setTimeout(function(){
+        var audio = new Audio(tsukuyomi);
+        audio.play();
+    })
+
 }
 
 const clock = new THREE.Clock();
@@ -92,23 +80,10 @@ document.onkeyup = function(e){
     keys[e.keyCode] = false;
 };
 
-function rotateAboutWorldAxis(object, axis, angle) {
-    var rotationMatrix = new THREE.Matrix4();
-    rotationMatrix.makeRotationAxis( axis.normalize(), angle );
-    var currentPos = new THREE.Vector4(object.position.x, object.position.y, object.position.z, 1.5);
-    var newPos = currentPos.applyMatrix4(rotationMatrix);
-    object.position.x = newPos.x;
-    object.position.y = newPos.y;
-    object.position.z = newPos.z;
-}
 
 function animate() {
     requestAnimationFrame(animate);
 
-    earth.rotation.y += 10; // rotate around its own axis
-
-    var yAxis = new THREE.Vector3(0, 20,0);
-    rotateAboutWorldAxis(earth,yAxis,Math.PI / 180);
 
     var delta = clock.getDelta();
     var speed = 10;
